c609db545cc6788f8824082e8312da17b3fd9342
Zachary-collins7 <Zachary.collins1231@gmail.com>
2023-10-12 03:18:50 -0600

    add util scripts

---
+++ src/scripts/util/hack.ts
@@ -0,0 +1,7 @@
+import { NS } from "ns";
+
+export async function main(ns: NS) {
+    for (let i = 0; i <= (ns.args[1] as number) || 1; i++) {
+        await ns.grow(ns.args[0] as string);
+    }
+}
---
+++ src/scripts/util/weaken.ts
@@ -0,0 +1,5 @@
+import { NS } from "ns";
+
+export async function main(ns: NS) {
+    await ns.weaken(ns.args[0] as string);
+}
